c68fa8b1430740fe7974f668fd5f02504acba104
Salman Malik <54137065+salman-ibrahim@users.noreply.github.com>
2021-08-02 19:10:47 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ require 'header.php';
 echo '<main id="main-site">';
 
 // Banner Area
-require '/templates/_banner_area.php';
+include 'templates/_banner_area.php';
 // Best Selling
-require '/templates/_best_selling.php';
+require 'templates/_best_selling.php';
 // Hot Products
-require '/templates/_hot_products.php';
+require 'templates/_hot_products.php';
 // Promotions
-require '/templates/_promotions.php';
+require 'templates/_promotions.php';
 // New and Trending
-require '/templates/_new_and_trending.php';
+require 'templates/_new_and_trending.php';
 // Latest Blogs
-require '/templates/_blogs.php';
+require 'templates/_blogs.php';
 
 echo '</main>';
 // Main Site End
